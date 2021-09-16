2394bd458f6a1816f8a2a9bc52a0adefcae57b73
CKPOON0619 <ckpoon19890129@gmail.com>
2021-09-16 13:05:52 +0100

    changes to imports

--- contentQuery/embeddings/USE.py
+++ contentQuery/embeddings/USE.py
@@ -1,7 +1,8 @@
-## Import for embeddings
+# Import for embeddings
 import tensorflow_hub as hub
 import tensorflow_text as text
 # The 16-language multilingual module is the default but feel free
 # to pick others from the list and compare the results.
-module_url = 'https://tfhub.dev/google/universal-sentence-encoder-multilingual/3' #@param ['https://tfhub.dev/google/universal-sentence-encoder-multilingual/3', 'https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3']
+# @param ['https://tfhub.dev/google/universal-sentence-encoder-multilingual/3', 'https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3']
+module_url = 'https://tfhub.dev/google/universal-sentence-encoder-multilingual/3'
 USE_embed = hub.load(module_url)
---
+++ contentQuery/tests/context.py
@@ -0,0 +1,10 @@
+# pylint: skip-file
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+    
+import embeddings
+import lsh
