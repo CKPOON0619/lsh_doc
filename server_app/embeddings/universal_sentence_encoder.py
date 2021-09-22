738ea7a2671f0640d5c6d6ea1e2a5bd80ff57e11
CKPOON0619 <ckpoon19890129@gmail.com>
2021-09-22 12:03:30 +0100

    Fixing linting issues

--- server_app/embeddings/universal_sentence_encoder.py
+++ server_app/embeddings/universal_sentence_encoder.py
@@ -8,5 +8,5 @@ import tensorflow_hub as hub
 # The 16-language multilingual module is the default but feel free
 # to pick others from the list and compare the results.
 # @param ['https://tfhub.dev/google/universal-sentence-encoder-multilingual/3', 'https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3']
-module_url = 'https://tfhub.dev/google/universal-sentence-encoder-multilingual/3'
-USE_embed = hub.load(module_url)
+MODULE_URL = 'https://tfhub.dev/google/universal-sentence-encoder-multilingual/3'
+USE_embed = hub.load(MODULE_URL)
